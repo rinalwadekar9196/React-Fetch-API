b883ccee9360c91c47c4e74f87b4b6afbcf6a863
Rinal <rinalwadekar9196@gmail.com>
2024-04-10 23:56:54 +0530

    Fetched data in cards using react api.

--- my-app/src/UserList.js
+++ my-app/src/UserList.js
@@ -1,50 +1,45 @@
 import React ,{useState,useEffect} from 'react'
 
-function UserList() {
-    const [user,setUser] = useState([]);
 
-    const fetchData =()=>{
-        fetch("https://randomuser.me/api/?nat=us&results=18&page=1")
-        //    fetch("https://restcountries.com/v3.1/all")
-        .then((response) =>{
-            return response.json();
-        }).then((objectData)=>{
-            let gagan = objectData.results
-            console.log(gagan);
-            setUser(gagan)
-        })
-    }
-    useEffect(()=>{
-        fetchData();
-    },[])
+const UserList = () => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setUser(data);
+      });
+  }, []);
 
-    return (
+return (
         <div className="clearfix">
         <div className="row">
           {user.map(data => (
-            <div className="col-md-4 animated fadeIn" key={data.id.value}>
+            <div className="col-md-4 animated fadeIn" >
               <div className="card">
                 <div className="card-body">
                   <div className="avatar">
                     <img
-                      src={data.picture.large}
+                      src={data.flags.png}
                       className="card-img-top"
                       alt=""
                     />
                   </div>
                   <h5 className="card-title">
-                    {data.name.first +
+                    {data.name.common +
                       " " +
-                      data.name.last}
+                      data.name.official}
                   </h5>
-                  <p className="card-text">
-                    {data.location.city +
-                      ", " +
-                      data.location.state}
-                    <br />
-                    <span className="phone">{data.phone}</span>
-                  </p>
+                  <div>
+                    {data.capital
+                    }
+                  </div>
+               <div>{data.timezones}</div>
                 </div>
+                <div>{data.continents}</div>
               </div>
             </div>
           ))}
@@ -53,6 +48,5 @@ function UserList() {
       </div>
     )
 }
-
 export default UserList
 
